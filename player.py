d838d1c967dc136aa80b009bb8c2b92a86624e1a
Spitfire1970 <zcabgoy@ucl.ac.uk>
2022-11-22 10:57:36 +0000

    Improved weight values

--- player.py
+++ player.py
@@ -304,6 +304,26 @@ class MyPlayer2(Player):
                             break
         return holes
 
+    def count_intell_holes(self, board):
+        holes = 0
+        intell_holes = []
+        for i in range(10):
+            for j in range(24):
+                if (i, j) not in board.cells:
+                    for s in range(0, j):
+                        if (i, s) in board.cells:
+                            intell_holes.append((i, j))
+                            break
+        for i in range(10):
+            for j in range(24):
+                n = 0.5
+                if (i, j) in intell_holes:
+                    for r in range(j +1, 24):
+                        if (i ,r) in intell_holes:
+                            n += 0.5
+                    holes += n
+        return holes
+
     def max_height(self, board):
         max_height = 25
         for cell in board.cells:
@@ -356,13 +376,13 @@ class MyPlayer2(Player):
         cells_left = 240 - len(board.cells)
 
         if max_height>13 and cells_left < 105:
-            score = score - ((holes*105) + (max_height*30) + (bumpiness*70) + ((rows_cleared))*0 + (pillars)*25)
+            score = score - ((holes*115) + (max_height*30) + (bumpiness*75) + ((rows_cleared))*0 + (pillars)*25)
         # if max_height>5 and cells_left < 180:
         #     score = score - ((holes*120) + (max_height*15) + (bumpiness*25)+ ((rows_cleared-4))*10 + (pillars)*0)
         elif max_height>8 and cells_left < 160:
-            score = score - ((holes*130) + (max_height*10) + (bumpiness*45) + ((rows_cleared))*0 + (pillars)*110)
+            score = score - ((holes*140) + (max_height*10) + (bumpiness*45) + ((rows_cleared))*45 + (pillars)*110)
         else:
-            score = score - ((holes*160) + (max_height*1) + (bumpiness*2) + ((rows_cleared))*60 + (pillars)*70)
+            score = score - ((holes*170) + (max_height*1) + (bumpiness*2) + ((rows_cleared))*80 + (pillars)*70)
         if rows_cleared == 4 or rows_cleared == 4.0:
             score =10000
         # print("Height:",max_height)
@@ -386,9 +406,10 @@ class MyPlayer2(Player):
                         for cell in board.falling.cells:
                             k = cell[0]
                             if k == i:
-                                le_moves.append(Direction.Drop)
-                                board.move(Direction.Drop)
-                                return le_moves
+                                while True:
+                                    le_moves.append(Direction.Down)
+                                    if board.move(Direction.Down):
+                                        return le_moves
                             if k<i:
                                 while True:
                                     le_moves.append(Direction.Right)
@@ -440,10 +461,11 @@ class MyPlayer2(Player):
                     # print(board.cells)
                     return le_moves
 
-        le_moves.append(Direction.Drop)
-        board.move(Direction.Drop) 
-        # print(board.cells)
-        return le_moves
+        while True:
+            le_moves.append(Direction.Down)
+            if board.move(Direction.Down): 
+            # print(board.cells)
+                return le_moves
 
     def move_to_target_rot(self, target_rot, target_pos, board):
         # global li
@@ -458,9 +480,10 @@ class MyPlayer2(Player):
                         for cell in board.falling.cells:
                             k = cell[0]
                             if k == i:
-                                le_moves.append(Direction.Drop)
-                                board.move(Direction.Drop)
-                                return le_moves
+                                while True:
+                                    le_moves.append(Direction.Down)
+                                    if board.move(Direction.Down):
+                                        return le_moves
                             if k<i:
                                 while True:
                                     le_moves.append(Direction.Right)
@@ -511,10 +534,11 @@ class MyPlayer2(Player):
                 if (l_block_start_pos == target_pos[0]) or (r_block_start_pos == 9):
                     break
         
-        le_moves.append(Direction.Drop)
-        board.move(Direction.Drop) 
-        # print(board.cells)
-        return le_moves
+        while True:
+            le_moves.append(Direction.Down)
+            if board.move(Direction.Down): 
+            # print(board.cells)
+                return le_moves
 
     def choose_action(self, board):
         global block_num
@@ -522,7 +546,7 @@ class MyPlayer2(Player):
         block_num +=1
         fin_prev_num_cell = 0
         print("Block", block_num)
-        #time.sleep(2)
+        #time.sleep(0.5)
         self.print_board(board)
         prev_holes = 0
         curr_holes = 0
